93ed05bb237fc3636aa04066442618cf67234ae2
Azorlolo <lolo.coppolino@gmail.com>
2025-07-22 15:16:41 +1100

    animations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import gsap from 'gsap';
 
-let skewSetter = gsap.quickTo("img", "skewY"), // fast
-	  clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees.
-
-ScrollSmoother.create({
-	wrapper: "#wrapper",
-	content: "#content",
-	smooth: 2,
-  speed: 3,
-	effects: true,
-	onUpdate: self => skewSetter(clamp(self.getVelocity() / -50)),
-	onStop: () => skewSetter(0)
-});
+// Animation for the main
+//fait une timeline pour les animations qui sont déja faites
+const tl = gsap.timeline({ defaults: { duration: 1, ease: "power2.out" } });
+tl.from("h1", { y: -100, opacity: 0 })
+    .from("#pp", {duration: 2, x: -100, rotation: 90, opacity: 0, ease: "power2.out"})
+    //faire gauche et droite en même temps
+    .from("#gauche", {duration: 1, x: -100, opacity: 0, ease: "power2.out"})
+    .from("#droite", {duration: 1, x: 100, opacity: 0, ease: "power2.out"}, "<")
+    .from("footer", { duration: 2, y: 100, opacity: 0, ease: "power2.out" });
